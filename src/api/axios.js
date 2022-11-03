4562803eb175b6ca6af98e3d9033c5cce4440481
obayalhusein <obayalhusein@gmail.com>
2022-11-03 16:14:49 +0300

    change content-type in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.covid19api.com';
 axios.defaults.headers.common['Accept'] = 'application/json';
-axios.defaults.headers.post['Content-Type'] = 'x-www-form-urlencoded';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export default axios;
